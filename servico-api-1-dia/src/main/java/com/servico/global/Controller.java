4d2af6e4433234ec96a9f91a32c07e77e44cc268
unknown <cirellia5@gmail.com>
2020-10-05 10:04:46 -0300

    aplicacoes java spring

--- servico-api-1-dia/src/main/java/com/servico/global/Controller.java
+++ servico-api-1-dia/src/main/java/com/servico/global/Controller.java
@@ -3,7 +3,9 @@ package com.servico.global;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,4 +29,10 @@ public class Controller {
 		repository.save(model);
 		return model;
 	} 
-}
+	
+	@DeleteMapping("/servicos/{id}")
+	public String remover (@PathVariable Long id ) {
+		repository.deleteById(id);
+	return "sucesso"; 
+	} 
+}  
