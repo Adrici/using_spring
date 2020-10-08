e3d64ce6e0d60334a42423f2a914c97544659c5f
unknown <cirellia5@gmail.com>
2020-10-08 13:20:13 -0300

    aplicacoes java_spring

--- LojaGames/src/main/java/com/loja/games/model/UsuarioModel.java
+++ LojaGames/src/main/java/com/loja/games/model/UsuarioModel.java
@@ -7,7 +7,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
-
+ 
 @Entity
 @Table(name = "usuario")
 public class UsuarioModel {
